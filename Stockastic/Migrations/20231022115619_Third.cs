4f17f6cf7cd96752cf39bbfe2c943a3323d4a407
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2023-10-22 09:02:48 -0300

    Migration para atualizar o tipo da propriedade PrazoValidade
    Mudei o tipo de PrazoValidade de DateTime para string por motivos de problemas na formatação.

---
+++ Stockastic/Migrations/20231022115619_Third.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stockastic.Migrations
+{
+    /// <inheritdoc />
+    public partial class Third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Produtos",
+                keyColumn: "PrazoValidade",
+                keyValue: null,
+                column: "PrazoValidade",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrazoValidade",
+                table: "Produtos",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PrazoValidade",
+                table: "Produtos",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
